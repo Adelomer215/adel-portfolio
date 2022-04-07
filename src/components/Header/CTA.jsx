ac80897336897cd557f7924eed5b81f2dcaba968
Adelomer215 <adelimer5533@gmail.com>
2022-04-07 03:34:48 +0200

    some modifing

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="adel-omer.pdf" download className="btn">
+      <a href="adel-omer.docx" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn-primary">
